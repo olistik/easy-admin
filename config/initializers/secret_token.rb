c76dcdc337357694e4ee3186cf68409c010c293a
Maurizio De Magnis <maurizio.demagnis@gmail.com>
2013-12-06 10:35:13 +0100

    genesis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EasyAdmin::Application.config.secret_key_base = '04cf7583bfdee8395b7d6fd668d661beaaf1180f602241a75487d7ae093f42c43d2b96d177f1312f53f658eae88240813a536fbe44a658559b54eb9e7a484633'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EasyAdmin::Application.config.session_store :cookie_store, key: '_easy-admin_session'
